cc9942400d00ed8372a6d4f894621c0ba8f7b135
JahnaviUK <preethibajjappa@gmail.com>
2025-05-07 11:42:55 +0530

    percentage

---
+++ percentage.py
@@ -0,0 +1,8 @@
+math = int(input("maths:"))
+english = int(input("english:"))
+science = int(input("science:"))
+sst = int(input("sst:"))
+sum = math+english+science+sst
+print("sum of the marks", sum)
+per = (sum / 400)*100
+print("percentage = ", per)
