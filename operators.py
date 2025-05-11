5fd85429ff744b72edb379ceaa64589ca81db12a
JahnaviUK <preethibajjappa@gmail.com>
2025-05-11 19:18:30 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,28 @@
+#additions
+num1 = 10 #int(input("Enter value for num1:"))
+num2 = 5 #int(input("Enter value for num2:"))
+print("sum of two numbers is :", num1 + num2)
+print("difference of two numbers is :",num1 - num2)
+print("product of two numbers is :",num1 * num2)
+print("quotient of two numbers is :", num1 / num2)
+
+
+print("\nis 50 < 45", 5 < 2)
+print("is 90 > 89", 5 > 2)
+print("is 100 = 100", 5 == 5)
+print ("is 23 >= 22", 23 >= 22)
+print("is 23 <= 22", 23 <= 22)
+print("23 not equal to 23", 23 != 23)
+
+a = 25
+b = 35
+c = a + b
+print(c)
+
+a = 25
+a+=35
+print("\n+=",a)
+a-=25
+print("-=", a)
+a*=10
+print("*=", a)
