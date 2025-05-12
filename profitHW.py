b2f73995a85600407bbfe57d3316159d13e5e098
JahnaviUK <preethibajjappa@gmail.com>
2025-05-12 12:37:29 +0530

    profitHW

---
+++ profitHW.py
@@ -0,0 +1,6 @@
+actualcost = float (input("enter the actual price - "))
+saleamount = float (input("enter the selling price - "))
+if (saleamount> actualcost):
+    print("profit")
+else : 
+    print ("loss")
