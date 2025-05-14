cf284bb26c8daae8466726a842780c998b291c16
JahnaviUK <preethibajjappa@gmail.com>
2025-05-14 11:31:29 +0530

    and operator

---
+++ and.py
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+c = 0
+if a and b and c :
+    print("all the numbers have boolean value as true")
+else :
+    print("atleast one number has boolean value as false")
+if a < 0 or b < 0:
+    print ("either of the numbers is greater than zero")
+else:
+    print("no number is greater than 0")
