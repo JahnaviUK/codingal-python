f62bf5e7b0ff9d149356da3c42d619e6290514ea
JahnaviUK <preethibajjappa@gmail.com>
2025-05-17 12:31:14 +0530

    elif

---
+++ elif.py
@@ -0,0 +1,12 @@
+mark = float(input("enter your marks in numbers"))
+print ("your marks is ", mark)
+if mark >= 95:
+    print("you will get 10,000 robux")
+elif mark >= 90:
+    print(" you will get a cycle")
+elif mark >= 85:
+    print(" you will get a new swimming gear")
+elif mark >= 80:
+    print(" you will get a bag of choclates")
+else: 
+    print(" you will get grounded")  
