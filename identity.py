392623576d41c1e98c168c72584bd03398c7d6ec
JahnaviUK <preethibajjappa@gmail.com>
2025-05-19 12:16:33 +0530

    identify

---
+++ identity.py
@@ -0,0 +1,14 @@
+x = 10
+if (type(x) is int) :
+    print("true")
+else :
+    print("false")
+x = 6.5
+if (type(x) is not float) :
+    print("true")
+else :
+    print("false")
+x = 20
+y = 20
+if x is y :
+    print("x and y are same ")
