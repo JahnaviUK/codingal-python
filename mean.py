05f20c432c43bc10095013109c205b7e808cf26b
JahnaviUK <preethibajjappa@gmail.com>
2025-05-21 11:36:34 +0530

    mean

---
+++ mean.py
@@ -0,0 +1,9 @@
+avg_1 = 38
+wrong_number= 36
+correct_number = 56
+total_number = 40
+sum = avg_1 * total_number
+correct_sum = sum - ((wrong_number) - (correct_number))
+print("correct sum", correct_sum)
+avg_2 = correct_sum / total_number
+print("correct average",avg_2)
