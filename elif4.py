b032e27761ec956530bd4d9f07a41edc9f295e16
JahnaviUK <preethibajjappa@gmail.com>
2025-05-22 10:58:51 +0530

    toys

---
+++ elif4.py
@@ -0,0 +1,11 @@
+toys = float(input("enter number of dogs toys"))
+print("your number of toys is", toys)
+if toys <= 1:
+    print("you will get 1 more dog toy free")
+elif toys <= 2:
+    print("you will get a costume for you dog for free") 
+elif toys <= 3:
+    print("you will get 2 more dog toy free") 
+elif toys <= 7:
+    print("you will get 1KG of dog kibble for free") 
+      
