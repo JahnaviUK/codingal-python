473cd55b51f77f59ffb7df6eaa7b474feeb3f569
JahnaviUK <preethibajjappa@gmail.com>
2025-06-09 16:20:19 +0530

    nested

---
+++ nested.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+if age >= 10:
+    if age <= 20:
+        print("Age is between 10 and 20 years.")
+    else:
+        print("Age is not between 10 and 20 years.")
+else:
+    print("Age is not between 10 and 20 years.")
