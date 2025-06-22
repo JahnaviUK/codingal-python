2282ec113766f31a4714af8eb2fa440b7c2e1d14
JahnaviUK <preethibajjappa@gmail.com>
2025-06-22 20:46:10 +0530

    square

---
+++ turtlesquare.py
@@ -0,0 +1,13 @@
+import turtle 
+turtle.Screen().bgcolor("pink")
+turtle.Screen().setup(300,400)
+x = turtle.Turtle()
+x.forward(100)
+x.right(90)
+x.forward(100)
+x.right(90)
+x.forward(100)
+x.right(90)
+x.forward(100)
+x.right(90)
+
