eb7679783db2dc3788b3264eebda6ab36812c00c
JahnaviUK <preethibajjappa@gmail.com>
2025-07-30 16:54:31 +0530

    set1

---
+++ sets.py
@@ -0,0 +1,14 @@
+myset = {1,2,3}
+print(myset)
+myset = {1.0,"momo",(1,2,3)}
+print(myset)
+myset = {1,2,3,4,3,2,1}
+print(myset)
+myset = set([1,2,3,4,5])
+print(myset)
+myset.pop()
+print("after removing the first element from the set",myset)
+myset.pop()
+print("after removing the second element from the set",myset)
+myset.add(15)
+print(myset)
